18802d31f10dd36418afc9981c652a3a67c0da70
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-09 22:53:05 +0000

    Create 2-New Year.cpp

---
+++ 2-New Year.cpp
@@ -0,0 +1,17 @@
+/*
+ * 2- New Year
+ * Link: https://vjudge.net/problem/AtCoder-abc084_a
+ */
+
+ #include <iostream>
+ using namespace std;
+
+ int main()
+ {
+    int x;
+
+    cin >> x;
+    cout << 48 - x;
+
+    return 0;
+ }
