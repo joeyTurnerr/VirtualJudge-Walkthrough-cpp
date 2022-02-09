f694f061e650ca4b1bb52232e5bae92f37da317c
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-09 22:34:59 +0000

    Create 1-Brick.cpp

---
+++ 1-Brick.cpp
@@ -0,0 +1,16 @@
+/*
+ * 1-Brick
+ * Link: https://vjudge.net/problem/AtCoder-abc186_a
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    
+    cin >> x >> y;
+    cout << x/y ;
+    
+    return 0;
+}
