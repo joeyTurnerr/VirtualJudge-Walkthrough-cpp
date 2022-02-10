ea92ac00c90da6a13c76d7604f721cd0ab7b0d87
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-10 00:15:35 +0000

    Create 5-box.cpp

---
+++ 5-box.cpp
@@ -0,0 +1,17 @@
+/*
+ * 5-box
+ * Link: https://vjudge.net/problem/AtCoder-abc180_a
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+
+    cin >> x >> y >> z;
+    cout << (x-y) + z;
+
+    return 0;
+}
