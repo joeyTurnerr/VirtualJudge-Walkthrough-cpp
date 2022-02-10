313737c27bcfabe15009c03a267344b6ec96bcf4
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-10 20:45:04 +0000

    Create 10-Watermelon.cpp

---
+++ 10-Watermelon.cpp
@@ -0,0 +1,20 @@
+/*
+ * 10-Watermelon
+ * Link: https://vjudge.net/problem/CodeForces-4A
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int w;
+    cin>> w;
+
+    if ( w%2==0 & w!=2)
+        cout << "YES";
+    else
+        cout<<"NO";
+
+    return 0;
+}
