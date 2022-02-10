a2b145ce63b3be10eee65e819abf63310cf29140
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-10 00:26:57 +0000

    Create 6-Discount Fare.cpp

---
+++ 6-Discount Fare.cpp
@@ -0,0 +1,15 @@
+/*
+ * 6-Discount Fare
+ * Link: https://vjudge.net/problem/AtCoder-abc113_a
+ */
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int x,y;
+
+    cin >> x >> y;
+    cout << x + float(0.5 * y);
+    return 0;
+}
