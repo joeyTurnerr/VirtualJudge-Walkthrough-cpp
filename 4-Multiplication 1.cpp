afc6f73e1b823600243595021c34a05fcb7f01b5
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-10 00:06:46 +0000

    Create 4-Multiplication 1.cpp

---
+++ 4-Multiplication 1.cpp
@@ -0,0 +1,17 @@
+/*
+ * 4-Multiplication 1
+ * Link: https://vjudge.net/problem/AtCoder-abc169_a
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,y;
+
+    cin >> x >> y;
+    cout << x*y;
+
+    return 0;
+}
