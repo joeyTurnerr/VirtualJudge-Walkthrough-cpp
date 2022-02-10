379497640becdd0bf1b057bcd484ed4e56987d53
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-10 01:00:35 +0000

    Create 8-kcal.cpp

---
+++ 8-kcal.cpp
@@ -0,0 +1,17 @@
+/*
+ * 8-kcal
+ * Link: https://vjudge.net/problem/AtCoder-abc205_a
+ */
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int a,b;
+
+    cin >> a >> b;
+    cout << (double)a/100 * b;
+
+    return 0;
+}
