7e20660ebef91aadbebcb0b1643d8caebb575025
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-12 03:59:51 +0000

    Create 16-Day of Takahashi.cpp

---
+++ 16-Day of Takahashi.cpp
@@ -0,0 +1,15 @@
+/*
+ * 16-Day of Takahashi
+ * Link: https://vjudge.net/problem/AtCoder-abc096_a
+ */
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int a, b;
+
+    cin >> a >> b;
+    (a<=b)? cout << a : cout << a-1;
+    return 0;
+}
