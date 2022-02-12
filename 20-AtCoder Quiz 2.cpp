5f96b94adda25794def304fd311d08cc90ed42f7
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-12 06:15:52 +0000

    Create 20-AtCoder Quiz 2.cpp

---
+++ 20-AtCoder Quiz 2.cpp
@@ -0,0 +1,24 @@
+/*
+ * 20-AtCoder Quiz 2
+ * Link: https://vjudge.net/problem/AtCoder-abc219_a
+ */
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int x;
+
+    cin>>x;
+
+    if (x>= 90)
+        cout << "expert";
+    else if (x>=70 && x<90)
+        cout << 90 - x;
+    else if (x>=40 && x<70)
+        cout << 70 -x;
+    else if (x>=0 && x<40)
+        cout << 40 -x;
+
+    return 0;
+}
