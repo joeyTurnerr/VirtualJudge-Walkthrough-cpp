f7f0958415fbb6bfb00fb93e72a291cb00fdb945
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-12 02:21:54 +0000

    Create 14-Even Odds.cpp

---
+++ 14-Even Odds.cpp
@@ -0,0 +1,20 @@
+/*
+ * 14-Even Odds
+ * Link: https://vjudge.net/problem/CodeForces-318A
+ */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+    long long n, k;
+    cin >> n >> k;
+
+    if ( n%2 == 0)
+        (k <= n/2)? cout << 2*k-1 : cout << 2*(k-n/2);
+
+    else
+        (k<= (n/2 +1))? cout << 2*k-1 : cout << 2*(k-(n/2+1));
+    return 0;
+}
