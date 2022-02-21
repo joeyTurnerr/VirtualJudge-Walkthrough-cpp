0bcd7d1eb9330955beeef996e8bfa548a457bb0d
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-21 23:59:04 +0000

    Create 27-Bit++.cpp

---
+++ 27-Bit++.cpp
@@ -0,0 +1,29 @@
+/*
+ * 27-Bit++
+ * Link: https://vjudge.net/problem/CodeForces-282A
+ */
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int x, counter{0};
+    string s;
+
+    cin >> x;
+    for (int i = 0; i < x; i++)
+    {
+        cin >> s;
+        if (s[0] == '+' || s[s.length()-1] == '+')
+        {
+            counter++;
+        }
+        else
+        {
+            counter--;
+        }
+    }
+    cout << counter;
+
+    return 0;
+}
