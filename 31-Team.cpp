8c7a48e7d435a9aece6b2e202b7bb1f01b3a6a9b
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-23 05:39:14 +0000

    Create 31-Team.cpp

---
+++ 31-Team.cpp
@@ -0,0 +1,26 @@
+/*
+ * 31-Team
+ * Link: https://vjudge.net/problem/CodeForces-231A
+ */
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int n, a, b, c, problem{0};
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a >> b >> c;
+        bool twice = (a == b && b == 1)  || (c == b && b == 1) || (a == c && c == 1) ;
+        bool full =  (a == b && b == c && a == 1);
+
+        if (twice || full)
+            problem++;
+    }
+    cout << problem;
+
+    return 0;
+}
