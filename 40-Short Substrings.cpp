88ac6863bc72180a1af1a83d34a20835861100e8
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-03-18 15:13:51 +0000

    Create 40-Short Substrings.cpp

---
+++ 40-Short Substrings.cpp
@@ -0,0 +1,20 @@
+//40-Short Substrings
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int t; cin >>t;
+    while (t--)
+    {
+        string s; cin >> s;
+        cout << s[0];
+        for (int i = 1; i < s.length(); i+=2)
+        {
+            cout << s[i];
+        }
+        cout <<"\n";
+    }
+
+    return 0;
+}
