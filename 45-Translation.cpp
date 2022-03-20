c043cf9b06729df5cce1eced979b0d2ac36e2d8d
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-03-20 17:45:00 +0000

    Create 45-Translation.cpp

---
+++ 45-Translation.cpp
@@ -0,0 +1,16 @@
+//45-Translation
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main ()
+{
+    string a,b;
+    cin >> a >> b;
+
+    reverse(b.begin(), b.end());
+
+    (a == b)? cout << "YES":cout <<"NO";
+
+    return 0;
+}
